17ef952553393f509232de5c1e607527393ff9b9
Ayaan Zeeshan <muhammad.ayaan.zeeshan123@gmail.com>
2024-11-07 15:46:01 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+//Homework:Footer(Create a footer and render it once so that it's available across all routes.)
+
+export default function Home(){
+  return(
+    <div>
+      <br />
+      <br />
+      <br />
+        <h1 className="text-5xl bg-black text-white text-center py-20 font-bold uppercase">
+          Welcome to Ayaan Zeeshan 
+        </h1>
+    </div>
+  )
+}
