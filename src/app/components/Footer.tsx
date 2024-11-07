2c422ffadbe5333db6835d634e24fd717f440fd7
Ayaan Zeeshan <muhammad.ayaan.zeeshan123@gmail.com>
2024-11-07 14:17:21 +0500

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,5 +1,5 @@
 //Homework:Footer(Create a footer and render it once so that it's available across all routes.)
-import Link from "next/link";
+
 const Footer = () => {
     return(
        <footer className="bg-gray-500 font-semibold text-center hover:text-black text-white ">
